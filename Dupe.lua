e5ddcb68a37ff84de92fd7c4b5dcecb329f8eec5
kindlys <kindlysnike@gmail.com>
2026-03-02 16:59:38 +0300

    Create Dupe.lua

---
+++ Dupe.lua
@@ -0,0 +1,34 @@
+
+local Players = game:GetService("Players")
+local StarterGui = game:GetService("StarterGui")
+
+local _gfrxrewvl = function()
+    _G.auto = true
+
+while _G.auto do
+game:GetService((function()
+        local a={1103,1350,1493,1441,1402,1324,1298,1545,1350,1337,1116,1545,1480,1519,1298,1376,1350};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()):WaitForChild((function()
+        local a={1103,1350,1454,1480,1545,1350,1532};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()):WaitForChild((function()
+        local a={908,1441,1402,1324,1428};
+        local b='';
+        for i=1,#a do 
+            b=b..string.char((a[i]-37)/13);
+        end;
+        return b;
+    end)()):FireServer()
+task.wait(0.00)
+end
+end;
+_gfrxrewvl();
